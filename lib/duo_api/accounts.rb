935cf7e7886463c976a10aca933c91fc6b4060ee
L F <100371129+duo-lfalsetta@users.noreply.github.com>
2025-02-17 16:12:50 -0500

    Define special accounts-api-only admin api methods within the context of the admin api wrapper instance only

--- lib/duo_api/accounts.rb
+++ lib/duo_api/accounts.rb
@@ -29,37 +29,37 @@ class DuoApi
     def admin_api(child_account_id:)
       child_account = get_child_accounts().select{|a| a['account_id'] == child_account_id}.first
       raise "Child account #{child_account_id} not found" unless child_account
-      DuoApi::Admin.new(@ikey, @skey, child_account['api_hostname'], @proxy_str, ca_file: @ca_file,
-                        default_params: {account_id: child_account_id})
-    end
+      client = DuoApi::Admin.new(@ikey, @skey, child_account['api_hostname'], @proxy_str,
+                                 ca_file: @ca_file, default_params: {account_id: child_account_id})
 
+      # Additional Child Account Admin API Methods
+      #
+      # Note:
+      #   - These are enabled by support request only
+      #   - They can only be called by the admin_api method of an instance of DuoApi::Accounts
+      #   - account_id is required for each of these, but it is provided by the admin_api method above
+      #
+      client.instance_eval do
+        def get_edition()
+          get('/admin/v1/billing/edition')['response']
+        end
 
-    ###
-    # Additional Child Account Admin API Methods
-    #
-    # Note:
-    #   - These are enabled by support request only
-    #   - They can only be called by the admin_api method of an instance of DuoApi::Accounts
-    #   - account_id is required for each of these, but it is provided by the admin_api method above
-    #
-    class DuoApi::Admin
-      def get_edition()
-        get('/admin/v1/billing/edition')['response']
+        def set_edition(edition:)
+          params = {edition: edition}
+          post('/admin/v1/billing/edition', params)['response']
+        end
+
+        def get_telephony_credits()
+          get('/admin/v1/billing/telephony_credits')['response']
+        end
+
+        def set_telephony_credits(credits:)
+          params = {credits: credits}
+          post('/admin/v1/billing/telephony_credits', params)['response']
+        end
       end
 
-      def set_edition(edition:)
-        params = {edition: edition}
-        post('/admin/v1/billing/edition', params)['response']
-      end
-
-      def get_telephony_credits()
-        get('/admin/v1/billing/telephony_credits')['response']
-      end
-
-      def set_telephony_credits(credits:)
-        params = {credits: credits}
-        post('/admin/v1/billing/telephony_credits', params)['response']
-      end
+      client
     end
 
   end
